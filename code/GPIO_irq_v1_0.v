bd311aa323df24f02cfb113dd6c15930b5342b76
wrxue <weirenxue@gmail.com>
2019-12-29 17:28:39 +0800

    change IP name

--- code/GPIO_irq_v1_0.v
+++ code/GPIO_irq_v1_0.v
@@ -1,7 +1,7 @@
 
 `timescale 1 ns / 1 ps
 
-	module myip_v1_0 #
+	module GPIO_irq_v1_0 #
 	(
 		// Users to add parameters here
 
@@ -45,10 +45,10 @@
 		input wire  s00_axi_rready
 	);
 // Instantiation of Axi Bus Interface S00_AXI
-	myip_v1_0_S00_AXI # ( 
+	GPIO_irq_v1_0_S00_AXI # ( 
 		.C_S_AXI_DATA_WIDTH(C_S00_AXI_DATA_WIDTH),
 		.C_S_AXI_ADDR_WIDTH(C_S00_AXI_ADDR_WIDTH)
-	) myip_v1_0_S00_AXI_inst (
+	) GPIO_irq_v1_0_S00_AXI_inst (
 		.S_AXI_ACLK(s00_axi_aclk),
 		.S_AXI_ARESETN(s00_axi_aresetn),
 		.S_AXI_AWADDR(s00_axi_awaddr),
